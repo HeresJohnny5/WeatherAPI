5c0e752065c283b680209c719b578db6a6d86929
John Erickson <ericksonjohn08@hotmail.com>
2017-12-26 15:33:38 -0500

    Extract request functionality to geocode directory. Add callback to geocodeAddress function.

--- app.js
+++ app.js
@@ -11,19 +11,12 @@ const argv = require('yargs')
 	.help()
 	.argv;
 
-const address = argv.a;
+const geocode = require('./geocode/geocode');
 
-request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address),
-	json: true
-}, (error, response, body) => {
-	if (error) {
-		console.log('Unable to connect to Google servers.');
-	} else if (body.status === 'ZERO_RESULTS') {
-		console.log(`Unable to find address ${address}`)
-	} else if (body.status === 'OK') {
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-		console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+	if (errorMessage) {
+		console.log(errorMessage);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
 	}
 });
