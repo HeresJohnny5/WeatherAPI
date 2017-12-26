4cca338a66cf5498afb387e003945c3c36d273cd
John Erickson <ericksonjohn08@hotmail.com>
2017-12-26 14:34:18 -0500

    Add yargs package. Add user functionality to dynamically input data.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const request = require('request');
+const argv = require('yargs')
+	.options({
+		a: {
+			describe: 'Address',
+			demand: true,
+			alias: 'a',
+			string: true
+		}
+	})
+	.help()
+	.argv;
+
+const address = argv.a;
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=601%20clariton%20blvd%20street%20pittsburgh',
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address),
 	json: true
 }, (error, response, body) => {
   console.log('error:', error); 
