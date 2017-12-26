ac72844c635e7b6ea5806ba587711789ff31aeca
John Erickson <ericksonjohn08@hotmail.com>
2017-12-26 13:41:54 -0500

    Add output for address, lat, and lng.

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ request({
 }, (error, response, body) => {
   console.log('error:', error); 
   console.log('statusCode:', response && response.statusCode);
-  console.log('body:', JSON.stringify(body, undefined, 2)); 
+//  console.log('body:', JSON.stringify(body, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+	console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
