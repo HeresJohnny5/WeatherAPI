cba11aef607d1d0f2e9824b54e646f0781de1eea
John Erickson <ericksonjohn08@hotmail.com>
2017-12-28 15:12:43 -0500

    Add body-parser module. Add results view to view directory. Add post functionality after user inputs address.Change actualTemperature variable in weather file to apparentTemperature.

--- weather/weather.js
+++ weather/weather.js
@@ -13,7 +13,7 @@ var getWeather = (results, callback) => {
 			if (!error && response.statusCode === 200) {
 				callback(undefined, {
 					temperature: body.currently.temperature,
-					actualTemperature: body.currently.apparentTemperature
+					apparentTemperature: body.currently.apparentTemperature
 				});
 			} else {
 				callback('Unable to fetch weather.');
